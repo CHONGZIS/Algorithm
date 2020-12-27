ca5ef4a1d18e2fe27bca96d85fc29f3b3434f1d7
jinhuajian <1542941700@qq.com>
2020-12-27 22:43:55 +0800

    Revert "进入stash缓存区"
    This reverts commit 1d525429

--- src/array/ReconstructQueue.java
+++ src/array/ReconstructQueue.java
@@ -1,6 +1,7 @@
 package array;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * 406. 根据身高重建队列
@@ -25,49 +26,5 @@ public class ReconstructQueue {
             list.add(i[1], i);
         }
         return list.toArray(new int[list.size()][2]);
-    }
-
-    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
-        ArrayList<int[]> ans = new ArrayList<>();
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                ans.add(new int[]{i, j});
-            }
-        }
-
-        ans.sort((o1, o2) -> Math.abs(o1[0] - r0) + Math.abs(o1[1] - c0) - Math.abs(o2[0] - r0) - Math.abs(o2[1] - c0));
-        return ans.toArray(new int[ans.size()][2]);
-    }
-    public String removeKdigits(String num, int k) {
-        // 从左到右升序就是最小，想到单调栈
-        Stack<Character> stack = new Stack<>();
-        for (char c : num.toCharArray()) {
-            while (!stack.isEmpty() && stack.peek() > c && k > 0) {
-                stack.pop();
-                k--;
-            }
-            if (stack.isEmpty() && c == '0') {
-                continue;
-            }
-            stack.push(c);
-        }
-
-        while (k-- > 0 && !stack.isEmpty()) {
-            stack.pop();
-        }
-        if (stack.isEmpty()) {
-            return "0";
-        }
-        StringBuilder ans = new StringBuilder();
-        for (Character c : stack) {
-            ans.append(c);
-        }
-        return ans.toString();
-    }
-    public static void main(String[] args) {
-        String num = "10";
-        int k = 2;
-        int R = 2, C = 2, r0 = 0, c0 = 1;
-        new ReconstructQueue().removeKdigits(num, k);
     }
 }
